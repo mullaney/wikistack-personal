d752f232f83873159e02f6daa88ff34d774ce3fc
Kevin Kelly <kvinklly@gmail.com>
2018-01-27 12:48:10 -0600

    additional field options

--- models/index.js
+++ models/index.js
@@ -2,16 +2,38 @@ const Sequelize = require('sequelize');
 const db        = new Sequelize('postgres://localhost:5432/wikistackp');
 
 const User = db.define('user', {
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
 });
 
 const Page = db.define('page', {
-  title: Sequelize.STRING,
-  urlTitle: Sequelize.STRING,
-  content: Sequelize.TEXT,
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  urlTitle: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
   status: {
     type: Sequelize.ENUM('open', 'closed')
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
   }
 });
 
